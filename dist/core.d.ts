ceaa7de6ce54a24e7fd183b5fc0addd52f7f48ba
icedlee337 <lee.e.norris@gmail.com>
2023-03-09 13:03:57 -0600

    chore: giving up

---
+++ dist/core.d.ts
@@ -0,0 +1,9 @@
+export function fileTypeFromStream(stream: any): Promise<any>;
+export function fileTypeFromBuffer(input: any): Promise<any>;
+export function fileTypeFromTokenizer(tokenizer: any): any;
+export function fileTypeStream(readableStream: any, { sampleSize }?: {
+    sampleSize?: number;
+}): Promise<any>;
+export const supportedExtensions: Set<string>;
+export const supportedMimeTypes: Set<string>;
+//# sourceMappingURL=core.d.ts.map
---
+++ dist/supported.d.ts
@@ -0,0 +1,3 @@
+export const extensions: string[];
+export const mimeTypes: string[];
+//# sourceMappingURL=supported.d.ts.map
